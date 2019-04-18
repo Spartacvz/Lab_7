1ef014166dffa4655ce58bc6ff57dbc301bc538c
Your Name <you@example.com>
2019-04-17 19:50:36 -0500

    message

---
+++ PetDepot/PetDepot.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+    el: "#app",
+    data: {
+        siteName: "Vue.js Pet Depot",
+        product: {
+            id: 1001,
+            title: "Cat Food, 25lb bag",
+            description: "A 25 pound bag of "
+        }
+    }
+})
---
+++ StudentSign/StudentSignIn.js
@@ -0,0 +1,7 @@
+let app = new Vue({
+    el: "#app",
+    data: {
+        students: [ { name: 'A. Student', starID: 'aa1234aa', present: false},
+             { name: 'B. Student', starID: 'bb1234bb', present: true} ]
+    }
+})
